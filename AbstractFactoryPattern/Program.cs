67a03ea466e02562064be6c3642e9d1194b41e62
Andres Orav <andres.orav@pipedrive.com>
2018-12-08 12:34:48 +0200

    Added mixed product factory

--- AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/Program.cs
@@ -12,19 +12,24 @@ namespace AbstractFactoryPattern
             var lowerProductFactory = new LowerProductFactory();
             var lowerProducts = lowerProductFactory.GetProducts("One", "Two");
 
-            WriteProductNames(lowerProducts);
+            WriteProductNames(lowerProductFactory.GetType().Name, lowerProducts);
             
             var upperProductFactory = new UpperProductFactory();
             var upperProducts = upperProductFactory.GetProducts("One", "Two");
 
-            WriteProductNames(upperProducts);
+            WriteProductNames(upperProductFactory.GetType().Name, upperProducts);
+            
+            var mixedProductFactory = new MixedProductFactory();
+            var mixedProducts = mixedProductFactory.GetProducts("One", "Two");
+
+            WriteProductNames(mixedProductFactory.GetType().Name, mixedProducts);
         }
 
-        private static void WriteProductNames(IEnumerable<AbstractProduct> products)
+        private static void WriteProductNames(string factoryName, IEnumerable<AbstractProduct> products)
         {
             foreach (var product in products)
             {
-                Console.WriteLine("Product {0}", product.GetName());
+                Console.WriteLine("Product from {0}: {1}", factoryName, product.GetName());
             }
         }
     }
